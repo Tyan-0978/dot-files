29b44afc9d38e31447dc949d705a9047e64eba1e
Tyan-0978 <960007nathan@gmail.com>
2023-12-28 18:00:56 +0800

    Show untracked file information for git prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,5 +1,6 @@
 function fish_prompt
     set -U __fish_git_prompt_show_informative_status 1
+    set -U __fish_git_prompt_showuntrackedfiles 1
 
     set -l time (set_color green) [(date +%H:%M:%S)]
     set -l dir (set_color --bold cyan) (prompt_pwd)
