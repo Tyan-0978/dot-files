de81225884aeab2e67f14f899d7086b1d2973f18
Tyan-0978 <960007nathan@gmail.com>
2023-12-28 10:34:05 +0800

    Fix vi mode & change insert cursor

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,6 @@
 function fish_user_key_bindings
-    fish_vi_mode
+    fish_vi_key_bindings
+    set -U fish_cursor_insert line
     bind -M insert \cf accept-autosuggestion
     bind \cf accept-autosuggestion
 end
