a10a95363775368198a16dce5f4d96daf34aaa9d
Tyan-0978 <960007nathan@gmail.com>
2023-12-28 10:33:38 +0800

    Use informative git prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,4 +1,6 @@
 function fish_prompt
+    set -U __fish_git_prompt_show_informative_status 1
+
     set -l time (set_color green) [(date +%H:%M:%S)]
     set -l dir (set_color --bold cyan) (prompt_pwd)
     set -l vcs (set_color --bold yellow) (fish_vcs_prompt)
