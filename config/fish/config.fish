f30d7094cf3ba5ac47392f232dca9d2085afa23f
Tyan-0978 <960007nathan@gmail.com>
2024-01-04 11:41:01 +0800

    Config Python virtual env prompt

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,7 @@
 fish_config theme choose "Just a Touch"
 
 set -U fish_prompt_pwd_dir_length 0
+set -U VIRTUAL_ENV_DISABLE_PROMPT 1
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -3,11 +3,12 @@ function fish_prompt
     set -U __fish_git_prompt_showuntrackedfiles 1
 
     set -l time (set_color green) [(date +%H:%M:%S)]
+    set -l venv (set_color white) \((path basename $VIRTUAL_ENV)\)
     set -l dir (set_color --bold cyan) (prompt_pwd)
     set -l vcs (set_color --bold yellow) (fish_vcs_prompt)
     set -l pointer (set_color normal) '> '
 
     echo ''
-    echo $time $dir $vcs
+    echo $time $dir $vcs $venv
     echo $pointer
 end
