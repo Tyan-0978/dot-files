9c21100881481a85ed9cd0bb656727084e77f77a
Tyan-0978 <960007nathan@gmail.com>
2024-06-01 22:15:43 +0800

    Remove thefuck and bun path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -11,9 +11,3 @@ set -U VIRTUAL_ENV_DISABLE_PROMPT 1
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-thefuck --alias | source
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
