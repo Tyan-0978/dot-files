30e455fae1e0d15d5a39fe00e5b555c55284f565
Tyan-0978 <960007nathan@gmail.com>
2025-06-03 00:57:11 +0800

    Refine fish key bindings

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,9 @@
 function fish_user_key_bindings
     fish_vi_key_bindings
-    set -U fish_cursor_insert line
-    bind -M insert \cf accept-autosuggestion
-    bind \cf accept-autosuggestion
+
+    set --universal fish_cursor_default block
+    set --universal fish_cursor_insert line
+
+    bind \cn accept-autosuggestion
+    bind --mode insert \cn accept-autosuggestion
 end
